e863f22aa67d5b54423033bac18351e54bb84354
Shahbuland Matiana <ssmatian@uwaterloo.ca>
2022-06-02 03:17:51 -0400

    Start working on overall API object

--- backend/client/text_captions.py
+++ backend/client/text_captions.py
@@ -4,7 +4,23 @@ class TextCaptionClient(Client):
     def __init__(self, id : int):
         super(Client, self).__init__(self.id)
 
-    def handle_task(self):
-        # TODO: Implement this
-        # Should implement some way of giving the user the text and allowing them to caption it
+        self.waiting = False # waiting for user to send back captions
+
+    def handle_task(self) -> bool:
+        assert self.task is not None
+
+        if not self.waiting:
+            self.busy = True
+            # Create JSON payload from the information in the task that will be presentable to the user
+
+            data = {}
+
+            data['story'] = self.task.data.text
+            data['caption_index'] = self.task.data.caption_index
+            data['captions'] = self.task.data.captions
+
+            # TODO: Send the json payload to front end
         
+            return False
+        else:
+
