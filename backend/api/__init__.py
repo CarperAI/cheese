0f5758e62dc69590cd142e806079b7af14fbe752
shahbuland <smatiana1@gmail.com>
2022-06-03 18:44:27 -0400

    preliminary code for API

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -6,3 +6,62 @@ class CHEESEAPI:
         self.orch = orch_cls(**orch_kwargs)
         self.client_cls = client_cls
         self.model_cls = model_cls
+
+    def create_client(self, id : int, **kwargs):
+        """
+        Create a client instance with given id and any other optional parameters.
+        
+        :param id: A unique identifying number for the client.
+        :type id: int
+
+        :param kwargs: Any other parameters to be passed to the client constructor.
+        """
+        if self.client_cls is None:
+            raise Exception("No client class specified")
+
+        new_client = self.client_cls(id, **kwargs)
+        self.orch.set_client(new_client)
+    
+    def create_model(self, **kwargs):
+        """
+        Create instance of model for labelling assistance
+
+        :param kwargs: Any parameters to be passed to the model constructor.
+        """
+        raise NotImplementedError
+
+    def step(self):
+        """
+        Gets tasks back from clients, sends finished tasks back to pipeline, handles queued tasks, receives new ones if available.
+        """
+
+        # Order of priority:
+        # 1. Getting tasks back from BUSY clients or model
+        # 2. Sending finished tasks to pipeline
+        # 3. Handling tasks in queue
+        # 4. Receiving new tasks from pipeline
+
+        # Always prioritize getting tasks from clients done first
+        for client in self.orch.clients:
+            client.handle_task() 
+            
+        # Have orch receive tasks from any finished clients
+        self.orch.query_clients()
+
+        # Get completed tasks and send them to pipeline
+        for task in self.orch.get_done_tasks():
+            self.pipeline.receive_task(task)
+        
+        # Handle tasks in queue
+        self.orch.handle_task()
+
+        # If orch has room for more tasks, take some from pipeline 
+        if self.orch.is_free():
+            # Get 
+            task = self.pipeline.create_data_task()
+            if task is not None: 
+                self.orch.receive_task(task)
+
+
+    
+
