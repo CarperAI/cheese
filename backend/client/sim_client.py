bdd5b33e700c613a8f54a17c5a385bd0f7fbb43b
shahbuland <smatiana1@gmail.com>
2022-06-03 19:25:07 -0400

    Fixed to work under API

--- backend/client/sim_client.py
+++ backend/client/sim_client.py
@@ -5,20 +5,24 @@ from backend.client.states import ClientState as CS
 import random
 
 class SimClient(Client):
-    def __init__(self):
-        self.id = int(random.random() * 1000)
-        super(SimClient, self).__init__(self.id)
+    def __init__(self, id : int = int(random.random() * 1000)):
+        super(SimClient, self).__init__(id)
     
-    def handle_task(self):
-        story = self.task.data.text
-        caption_inds = []
-        captions  = []
+    def handle_task(self) -> bool:
+        if self.state == CS.BUSY:
+            story = self.task.data.text
+            caption_inds = []
+            captions  = []
 
-        caption_inds = [[0, 5], [4, 7]]
-        captions = ["caption 1", "caption 2"]
-        
-        self.task.data.captions = captions
-        self.task.data.caption_index = caption_inds
-        self.task.receiver = TaskType.PIPELINE
-        self.task.sender = TaskType.USER
-        self.state = CS.IDLE
+            caption_inds = [[0, 5], [4, 7]]
+            captions = ["caption 1", "caption 2"]
+            
+            self.task.data.captions = captions
+            self.task.data.caption_index = caption_inds
+            self.task.receiver = TaskType.PIPELINE
+            self.task.sender = TaskType.USER
+            self.state = CS.IDLE
+            return True
+
+        else:
+            return False
