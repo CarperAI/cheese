5e6027b62a179b40027bdb29d1f904d50af390a6
shahbuland <smatiana1@gmail.com>
2022-06-03 19:23:49 -0400

    Finished API, further testing may be needed

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -30,9 +30,10 @@ class CHEESEAPI:
         """
         raise NotImplementedError
 
-    def step(self):
+    def step(self) -> bool:
         """
         Gets tasks back from clients, sends finished tasks back to pipeline, handles queued tasks, receives new ones if available.
+        Returns True once pipeline is exhausted and task queues are all empty.
         """
 
         # Order of priority:
@@ -49,18 +50,25 @@ class CHEESEAPI:
         self.orch.query_clients()
 
         # Get completed tasks and send them to pipeline
-        for task in self.orch.get_done_tasks():
-            self.pipeline.receive_task(task)
+        self.pipeline.receive_data_tasks(self.orch.get_completed_tasks())
         
         # Handle tasks in queue
         self.orch.handle_task()
 
-        # If orch has room for more tasks, take some from pipeline 
+        # If orch has room for more tasks, take some from pipeline
+        exhausted_pipe = False
         if self.orch.is_free():
             # Get 
             task = self.pipeline.create_data_task()
             if task is not None: 
                 self.orch.receive_task(task)
+            else:
+                exhausted_pipe = True
+
+        if exhausted_pipe and self.orch.get_total_tasks() == 0:
+            return True
+
+        return False
 
 
     
