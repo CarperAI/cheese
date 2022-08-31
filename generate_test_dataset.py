801ad567096ae21fa5d587047a41a87cafa4be5a
s <smatiana1@gmail.com>
2022-08-31 12:11:19 -0400

    Add script to generate test dataset for text captions

---
+++ generate_test_dataset.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from datasets import Dataset
+
+df = pd.DataFrame(["The goose swam to the shore.", "The goose took a nap.", "Then the goose swam away."], columns = ["text"])
+dataset = Dataset.from_pandas(df)
+df.save_to_disk("dataset")
