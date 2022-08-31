b0e61cb8eff220a1a8c30e5f44e729fdacebe5ed
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2022-08-31 16:05:30 -0400

    Update index route to provide a hint

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,11 +1,2 @@
-<script type="ts">
-    import Test from './test.svelte';
-
-    function handleHighlights(event:any){
-        let highlights = event.detail;
-        console.log("Recieved highlights", highlights);
-    }
-
-</script>
 <h1 class="text-xl">Welcome to Cheese!</h1>
-<Test on:highlights={handleHighlights}/>
+<p>Obtain a client ID to start.</p>
