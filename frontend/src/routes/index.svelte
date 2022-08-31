e822a4fd5f4210435e4525f21ae992fe8b9cf5a9
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2022-08-31 16:19:28 -0400

    Improve index route

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1 class="text-xl">Welcome to Cheese!</h1>
+<script>
+  import { goto } from '$app/navigation';
+  let clientID = "";
+</script>
+
+<h1 class="text-xl">Welcome to CHEESE!</h1>
 <p>Obtain a client ID to start.</p>
+
+<p>Already have a client ID? Enter it here:</p>
+<form on:submit|preventDefault={goto(`/client/${clientID}`)}>
+  <input type="text" bind:value={clientID} class="bg-zinc-200" />
+  <button disabled={!clientID} class="bg-black text-white disabled:bg-zinc-500">Go</button>
+</form>
