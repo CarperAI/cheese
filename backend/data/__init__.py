86df57500585a8d08a0854887898ac891dbefee5
s <smatiana1@gmail.com>
2022-09-09 13:33:08 -0400

    Update tasks and batch elements to store more info

--- backend/data/__init__.py
+++ backend/data/__init__.py
@@ -3,4 +3,7 @@ from dataclasses import dataclass
 @dataclass
 class BatchElement:
     """Abstract base class for BatchElements. Store all kinds of data being passed around CHEESE"""
-    pass
+    client_id : int = -1 # ID of the client that was assigned the data last
+    trip : int = 0 # How many targets has the BatchElement visited so far?
+    trip_max : int = 1 # How many targets *should* the BatchElement visit before going back to pipeline
+    # Default value of 1 causes data to immediately go back to pipeline after labelling by user
