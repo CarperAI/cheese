5e31835fd9153b9025e76a3c5ae8a03eff0b2328
s <smatiana1@gmail.com>
2022-10-13 00:32:21 -0400

    Add ability for model to batch tasks

--- cheese/models/__init__.py
+++ cheese/models/__init__.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from typing import Iterable, Any
 
 import pickle
 
@@ -9,7 +10,15 @@ from b_rabbit import BRabbit
 from cheese.utils.rabbit_utils import rabbitmq_callback
 
 class BaseModel:
-    def __init__(self):
+    """
+    BaseModel object handles anything that may require a model for processing data. It can also be used more generally
+    just to handle data processing separately from the pipeline and client.
+
+    :param batch_size: The maximum number of elements to process at once. If there are not this many elements available,
+        the model will simply process everything that is in the task queue.
+    :type batch_size: int
+    """
+    def __init__(self, batch_size : int = 1):
         self.publisher = None
         self.subscriber = None
 
@@ -35,10 +44,12 @@ class BaseModel:
         self.subscriber.subscribe_on_thread()
 
     @abstractmethod
-    def process(self, data : BatchElement) -> BatchElement:
+    def process(self, data : Iterable[BatchElement]) -> Iterable[BatchElement]:
         """
         Process BatchElement with model. Assume the inputs to the model are in the BatchElement,
         then use them to create some outputs. The outputs should be added to the BatchElement before it is returned.
+
+        :param data: The data to process. Can be an iterable of BatchElements, or a single one, depending on use-case.
         """
         pass
 
@@ -52,9 +63,16 @@ class BaseModel:
         self.working = True
 
         while self.task_queue:
-            task = self.task_queue.pop(0)
-            task.data = self.process(task.data)
-            self.queue_task(task)
+            tasks = self.task_queue[:self.batch_size]
+            self.task_queue = self.task_queue[self.batch_size:]
+
+            data_list = self.process([task.data for task in tasks])
+            for i, data in enumerate(data_list):
+                tasks[i].data = data
+            
+            while tasks:
+                self.queue_task(tasks[0])
+                tasks = tasks[1:]
 
         self.working = False
 
