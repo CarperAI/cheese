dd046593af076d093b9bd0597b190e80e863ce48
s <smatiana1@gmail.com>
2022-12-07 19:22:31 -0500

    Upgrade datasets pipeline to include csv datasets, add option to save every N steps instead of every step

--- cheese/pipeline/datasets.py
+++ cheese/pipeline/datasets.py
@@ -8,12 +8,22 @@ from cheese.pipeline import Pipeline
 class DatasetPipeline(Pipeline):
     """
     Base class for any pipeline thats data destination is a datasets.Dataset object
+    
+    :param format: Format to save result dataset to. Defaults to arrow. Can be arrow or csv.
+    :type format: str
+
+    :param save_every: Save dataset whenever this number of rows is added.
+    :type save_every: int
     """
-    def __init__(self):
+    def __init__(self, format : str = "csv", save_every  : int = 1):
         super().__init__()
 
         self.write_path : str = None
         self.res_dataset : Dataset = None
+        self.format = format
+        
+        self.save_every = save_every
+        self.save_accum = 0
 
     def save_dataset(self):
         """
@@ -25,7 +35,10 @@ class DatasetPipeline(Pipeline):
         if self.write_path is None:
             raise Exception("Error: Attempted to save result dataset without every specifiying a path to write to")
 
-        self.res_dataset.save_to_disk(self.write_path)
+        if self.format == "arrow":
+            self.res_dataset.save_to_disk(self.write_path)
+        elif self.format == "csv":
+            self.res_dataset.to_csv(self.write_path)
 
     def add_row_to_dataset(self, row : Dict[str, Any]):
         """
@@ -39,5 +52,9 @@ class DatasetPipeline(Pipeline):
             self.res_dataset = Dataset.from_dict(row)
         else:
             self.res_dataset = self.res_dataset.add_item(row)
-        self.save_dataset()
+
+        self.save_accum += 1
+        if self.save_accum >= self.save_every:
+            self.save_dataset()
+            self.save_accum = 0
 
