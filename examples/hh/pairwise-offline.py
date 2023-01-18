15c763bd5a9e68e9c6767d63efe1ec50b4d2ba29
Emerson Havener <emerson@nevada.unr.edu>
2023-01-17 16:51:11 -0800

    initial view for offline pairwise comparison UI

---
+++ examples/hh/pairwise-offline.py
@@ -0,0 +1,107 @@
+from cheese.data import BatchElement
+from dataclasses import dataclass
+
+@dataclass
+class SentimentElement(BatchElement):
+    prompt : str = None
+    first_output : str = None
+    second_output : str = None
+    label : str = None
+
+    trip_max : int = 2
+
+
+from cheese.pipeline.iterable_dataset import IterablePipeline
+
+
+class SentimentPipeline(IterablePipeline):
+    def preprocess(self, x):
+        return x # Don't need any changes here- it's just a string
+
+    def fetch(self) -> SentimentElement:
+        next_element = self.fetch_next()
+        # TODO: exit here if no element
+        return SentimentElement(
+            prompt=next_element["prompt"],
+            first_output=next_element["first_output"],
+            second_output=next_element["second_output"],
+        )
+
+    def post(self, data : SentimentElement):
+        row = {"prompt": data.prompt, "first_output": data.first_output, "second_output": data.second_output, "label": data.label}
+        print("posting row: ")
+        print(row)
+        if not data.error: self.add_row_to_dataset(row)
+
+
+from cheese.client.gradio_client import GradioFront
+import gradio as gr
+
+class SentimentFront(GradioFront):
+    def main(self):
+        with gr.Column():
+            prompt = gr.Textbox(label = "Prompt")
+            first_output = gr.Textbox(label = "First response")
+            second_output = gr.Textbox(label = "Second response")
+            label = gr.Radio(["First", "Second"], label = "Which response do you prefer?", visible = True)
+            btn = gr.Button("Submit")
+
+        self.wrap_event(btn.click)(
+            self.response, inputs = [label], outputs = [prompt, first_output, second_output, label]
+        )
+
+        return [prompt, first_output, second_output, label]
+
+    def receive(self, *inp):
+        # Receive gets a list of inputs which consist of
+        # [id, task, *inputs], where *inputs is the gradio inputs
+        # in this case, the gradio inputs are just the radio selection
+        _, task, label = inp
+        task.data.label = label
+        task.data.error = (label is None) # Error if the label wasn't selected
+
+        # We can choose to raise an InvalidInputException here if we want to
+        # By default, this would simply result in the same data being shown
+        # to the user again.
+        # For this example, we allow invalid inputs, but mark them as errors so they aren't written to
+        # result dataset
+
+        # Receive has to return the task updated with user input
+        return task
+
+    def present(self, task):
+        data : SentimentElement = task.data
+        return [data.prompt, data.first_output, data.second_output, data.label] # Return list for gradio outputs
+
+
+from cheese import CHEESE
+import time
+from datasets import load_dataset
+
+dataset = load_dataset("Dahoas/synthetic-instruct-gptj-pairwise", split="train")
+dataset = dataset.rename_column("chosen", "first_output")
+dataset = dataset.rename_column("rejected", "second_output")
+
+data = iter(dataset) # Cast to an iterator for IterablePipeline
+
+cheese = CHEESE(
+    pipeline_cls = SentimentPipeline,
+    client_cls = SentimentFront,
+    gradio = True,
+    pipeline_kwargs = {
+        "iter" : data,
+        "write_path" : "./pairwise_offline_result.csv",
+        "force_new" : False
+    }
+)
+
+print(cheese.launch()) # Prints the URL
+
+print(cheese.create_client(1)) # Create client with ID 1 and return a user/pass for them to use
+print(cheese.create_client(2))
+print(cheese.create_client(3))
+
+while not cheese.finished:
+    time.sleep(2)
+
+print("Done!")
